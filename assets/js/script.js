c18b692dbf6fe22d5ab91d71fe0369654d00ba64
mevine <mevine@free.fr>
2023-05-10 11:08:39 +0200

    calculatrice virgule

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let result = document.querySelector("#result")
 
 // On définit l'affichage par défaut = nb1
 let display = nb1
+let virgule = 0
 
 
 // on selectionne tous les éléments de classe .nb
@@ -14,7 +15,10 @@ const myNb = document.querySelectorAll('.nb')
 myNb.forEach(element => {
     // au click j'affiche le contenu du bouton (element.inner) dans display (display.innerText)
     element.addEventListener('click', function () {
+
         display.innerText += element.innerText
+
+
     })
 })
 
@@ -27,14 +31,35 @@ myOperator.forEach(element => {
     })
 })
 
-let btnac = document.getElementById("btnac") 
+let btnac = document.getElementById("btnac")
 
-btnac.addEventListener("click", function() {
+btnac.addEventListener("click", function () {
     nb1.innerText = ""
     operator.innerText = ""
     nb2.innerText = ""
     display = nb1
+    result.innerText = ""
 })
 
 
+btnequal.addEventListener("click", function () {
+    let operatorValue = operator.innerText
+    let resultat = 0
+    switch (operatorValue) {
+        case '+':
+            resultat = (Number(nb1.innerText) + Number(nb2.innerText))
+            break
+        case '-':
+            resultat = (Number(nb1.innerText) - Number(nb2.innerText))
+            break
+        case '/':
+            resultat = (Number(nb1.innerText) / Number(nb2.innerText))
+            break
+        case 'x':
+            resultat = (Number(nb1.innerText) * Number(nb2.innerText))
+            break
+    }
+    result.innerText = resultat
+
+})
 
